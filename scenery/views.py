81a7f8967dfce6fbf6e93357ee2249f03e5d0940
vovo <vovo@vovoui-MacBook-Pro.local>
2018-11-12 16:06:36 +0900

    Add comments

--- scenery/views.py
+++ scenery/views.py
@@ -6,8 +6,12 @@ from datetime import datetime, timedelta
 
 
 def main_view(request):
+    # get 으로 사진 텀, 날짜를 구해옴
+    # 없다면 기본텀은 10
     period = int(request.GET.get('period', '10'))
-    request_time = request.GET.get('time',None)
+    request_time = request.GET.get('time', None)
+
+    # 요청 날짜가 없으면 오늘이 기본
     if request_time is not None:
         request_time = str(request_time)
         year = int(request_time[:4])
@@ -20,6 +24,7 @@ def main_view(request):
     else:
         now = datetime.now()
 
+    # 사진이 5분단위로 존재 함으로 5분이 아닌 요청의 경우 5분단위로 설정
     m_int = period
     m_delta = timedelta(minutes=period)
     image_list = []
